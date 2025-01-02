cadbb18d4682bb5041d1f4d3a647c9bfe259a363
cinda87 <j.c.bietz@gmail.com>
2025-01-02 00:11:03 -0500

    Updated Date and Time JS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//update current temperature//
+//update current temperature
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
@@ -9,9 +9,12 @@ function refreshWeather(response) {
   let iconElement = document.querySelector("#icon");
   let pressureElement = document.querySelector("#pressure");
   let feelsLikeElement = document.querySelector("#feels-like");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windSpeedElement.innerHTML = response.data.wind.speed;
@@ -21,17 +24,56 @@ function refreshWeather(response) {
   feelsLikeElement.innerHTML = response.data.temperature.feels_like;
 }
 
+//Date/Time
+function formatDate(dateTime){
+
+let date = dateTime.getDate();
+let hours = dateTime.getHours();
+let minutes = dateTime.getMinutes();
+
+let days = [
+  "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat",
+];
+let day = days[dateTime.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "Septemper",
+  "October",
+  "November",
+  "December",
+];
+
+let month = months[dateTime.getMonth()];
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+return  `${day}, ${month} ${date} ${hours}:${minutes}`; 
+}
 
  
 
-//api//
+//api
 function searchCity(city) {
   let apiKey = "a050491735e3o6daf6dd43f3ab206bct";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
-//Search changes location title//
+//Search changes location title
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
