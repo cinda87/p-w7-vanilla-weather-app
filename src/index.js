0c13e922f9259cc402475e801f45c98eddbb3552
cinda87 <j.c.bietz@gmail.com>
2025-01-01 19:42:41 -0500

    Added api to update current temp

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
+//update current temperature//
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+//api//
+function searchCity(city) {
+  let apiKey = "a050491735e3o6daf6dd43f3ab206bct";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 //Search changes location title//
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let locationElement = document.querySelector("#location");
-    locationElement.innerHTML = searchInput.value;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value)
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Oslo");
