4bb151a4126cf04d215178da421fadfa3f262833
cinda87 <j.c.bietz@gmail.com>
2025-01-01 23:38:03 -0500

    Added toggle HTML & CSS

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let iconElement = document.querySelector("#icon");
+  let pressureElement = document.querySelector("#pressure");
+  let feelsLikeElement = document.querySelector("#feels-like");
+
 
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windSpeedElement.innerHTML = response.data.wind.speed;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+  pressureElement.innerHTML = response.data.temperature.pressure;
+  feelsLikeElement.innerHTML = response.data.temperature.feels_like;
 }
+
+
+ 
 
 //api//
 function searchCity(city) {
@@ -17,14 +33,15 @@ function searchCity(city) {
 
 //Search changes location title//
 function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
-    searchCity(searchInput.value)
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Oslo");
+
